958c3b465307a643f79bc6456c19c50a676d07b3
Liz Glasser <liz@corgibytes.com>
2021-06-30 10:03:17 -0400

    Initial checkin

---
+++ Corgibytes.CultureInformation/CultureInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Corgibytes.CultureInformation
+{
+    class CultureInformation
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo[] cinfo = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            foreach (CultureInfo cul in cinfo)
+            {
+                CultureInfo.CurrentCulture = cul;
+                string culNowString = DateTime.Now.ToString("yyyy/MM/dd");
+                Console.WriteLine("Code: {0}, EnglishName: {1}, Native Name: {2}", 
+                    CultureInfo.CurrentCulture.Name, CultureInfo.CurrentCulture.EnglishName, CultureInfo.CurrentCulture.NativeName);
+                Console.WriteLine("Current Date in yyyy/MM/dd format: {0}\n", culNowString);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CultureInformation
+
+This will loop over all cultures (including neutral cultures such as "es" and "en" which have no country component and the invariant country). It will display name information and the current date in the yyyy/MM/dd format. Here "/" is replaced with the date separator defined in the culture.
+
+This tool can be updated to display other information and is a useful aid in determining possible impact to date and numeric string values in an application.
+
+Note that this is setting the CurrentCulture, not the CurrentUICulture. The CurrentUICulture is used in conjunction with resource files to change displayed labels, etc.
